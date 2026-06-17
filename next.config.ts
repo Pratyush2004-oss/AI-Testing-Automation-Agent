176e701b6e3a9108bcc5eb150e8bd6f02b22fec7
Pratyush2004-oss <pratyush@sitp.ac.in>
2026-06-17 08:50:57 +0530

    Adding changes

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: [
-  "playwright",
-  "playwright-core",
-  "@browserbasehq/sdk",
-]
+    "playwright",
+    "playwright-core",
+    "@browserbasehq/sdk",
+  ],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/playwright-core/browsers.json"],
+  },
 };
 
 export default nextConfig;
